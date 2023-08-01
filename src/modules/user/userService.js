e4af1e5e573824a71327bbe7fe76821a193f93d0
Erick Fernandes <erick.gomes.fernandes.egf@gmail.com>
2023-08-01 19:54:18 -0300

    Criado tratativa de erros para signup

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -3,10 +3,8 @@ let users = []
 const getUserByEmail = (searchEmail) => users.find((obj) => obj.email === searchEmail)
 
 export const signup = (data) => {
-  if (getUserByEmail(data.email)) {
-    console.log("EXISTE O EMAIL")
-  } else {
-    users.push(data)
-  }
-  return true
+  if (getUserByEmail(data.email)) throw new Error('email_existente')
+   
+  users.push(data) 
+   return true
 }
